3be8bdfe63659aec6131165d6b46f7447e0db3ef
Marco Pivetta <ocramius@gmail.com>
2025-12-10 15:15:17 +0100

    Correct usage of `Scope` to always be passed with `NodeCallbackInvoker` in tests
    Since PHPStan `2.1.32`, `Scope` alone cannot be passed
    to `\PHPStan\Rules\Rule#processNode()`, which instead
    needs a `Scope&NodeCallbackInvoker`.
    
    Ref: https://github.com/phpstan/phpstan-src/commit/e271af4bfe2e7ddd2a4ce039447d93122f7356c8
    Ref: https://github.com/phpstan/phpstan-src/blob/f7ccea98010d132f30891af12a66a9b49b2596ba/src/Rules/Rule.php
    Ref: https://github.com/phpstan/phpstan-src/pull/4429

--- tests/src/ScopeWithNodeCallbackInvoker.php
+++ tests/src/ScopeWithNodeCallbackInvoker.php
@@ -7,6 +7,7 @@ namespace Roave\PHPStanTest\Rules\Floats;
 use PHPStan\Analyser\NodeCallbackInvoker;
 use PHPStan\Analyser\Scope;
 
+/** @phpstan-ignore phpstanApi.interface */
 interface ScopeWithNodeCallbackInvoker extends Scope, NodeCallbackInvoker
 {
 }
